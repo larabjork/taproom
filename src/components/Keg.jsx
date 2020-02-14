4b6cb2553395cc7c2ffd363acc50cdec34fdb22a
Lara Bjork <lara.m.bjork@gmail.com>
2020-02-14 11:21:08 -0800

    add masterKegList examples, propTypes for Keg

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -1,5 +1,26 @@
-// import React from 'react';
-//
-//
-//
-// export default Keg;
+import React from "react";
+import PropTypes from "prop-types";
+
+function Keg(props){
+  return (
+      <div>
+        <h3>{props.names}</h3>
+        <p><em>{props.brand}</em></p>
+        <p><em>{props.price}</em></p>
+        <p><em>{props.alcoholContent}</em></p>
+        <p><em>{props.pintRemaining}</em></p>
+        <hr/>
+      </div>
+  );
+}
+
+Keg.propTypes = {
+  names: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.number,
+  alcoholContent: PropTypes.number,
+  pintRemaining: PropTypes.number
+};
+
+
+export default Keg;
--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,5 +1,56 @@
-// import React from 'react';
-//
-//
-//
-// export default KegList;
+import React from 'react';
+import Keg from './Keg'
+
+const masterKegList = [
+  {
+    names: 'Scala',
+    brand: 'Anonymous Local Brewing Co.',
+    price: '4',
+    alcoholContent: '5%',
+    pintRemaining: '120'
+  },
+  {
+    names: 'Ruby',
+    brand: 'Sparkle Brewz',
+    price: '6'
+    alcoholContent: '4%',
+    pintRemaining: '120'
+  },
+    names: 'Java',
+    brand: 'Sunshine Brewing',
+    price: '6'
+    alcoholContent: '7%',
+    pintRemaining: '120'
+  },
+    names: 'Django',
+    brand: 'Sparkle Brewz',
+    price: '4'
+    alcoholContent: '4%',
+    pintRemaining: '120'
+  },
+  {
+    names: 'Fortran',
+    brand: 'Anonymous Local Brewing Co.',
+    price: '5'
+    alcoholContent: '7%',
+    pintRemaining: '120'
+  }
+];
+
+function KegList(){
+  return (
+  <div>
+    <hr/>
+    {masterKegList.map((keg, index) =>
+      <Keg names={keg.names}
+        brand={keg.brand}
+        price={keg.price}
+        alcoholContent={keg.alcoholContent}
+        pintRemaining={keg.pintRemaining}
+        key={index}/>
+    )}
+  </div>
+);
+
+
+export default KegList;
