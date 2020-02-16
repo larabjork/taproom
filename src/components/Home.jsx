a6c1ab51b9edcf14c088aa759a9955d9fcc4b035
Lara Bjork <lara.m.bjork@gmail.com>
2020-02-15 21:59:51 -0800

    swap images in and out, get rid of carousel for now

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,87 +1,19 @@
 import React from 'react';
-import Carousel  from 'react-bootstrap/Carousel';
-import bottles from '../assets/images/bottles.jpg';
-import cinti from '../assets/images/cinti.png';
-import empty from '../assets/images/empty-bar.jpg';
-import macleod from '../assets/images/macleod_tap.jpg';
-import outside from '../assets/images/outside_seating.jpg';
-import people from '../assets/images/two-people.jpg';
+import stein from '../stein.jpg';
+
 
 
 
 function Home(){
+
   return (
-    <>
-    <div className  ='carouselBody'>
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100 carouselImg"
-            src={bottles}
-            alt="Four brown glass bottles"
-            />
-          <Carousel.Caption>
-            <h3>We like to bring you the unusual</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block w-100 carouselImg"
-            src={empty}
-            alt="Empty bar"
-            />
-          <Carousel.Caption>
-            <h3>Pull up a seat!</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block w-100 carouselImg"
-            src={cinti}
-            alt="Historical print of brewery in Cincinnati, Ohio"
-            />
-          <Carousel.Caption>
-            <h3>Craft beer is in our DNA</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block w-100 carouselImg"
-            src={macleod}
-            alt="Person pouring a beer from a line of taps"
-            />
-          <Carousel.Caption>
-            <h3>Our staff is knowledgable and enthustiastic about brewcraft</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block w-100 carouselImg"
-            src={outside}
-            alt="Many people outside at picnic tables"
-            />
-          <Carousel.Caption>
-            <h3>When summer comes, our patio is the place to be</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block w-100 carouselImg"
-            src={people}
-            alt="Group of people sharing food and drink"
-            />
-          <Carousel.Caption>
-            <h3>Check out our food, too!</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+    <div className="homeBody">
+      <div>
+        <h1>Test for style</h1>
+      </div>
     </div>
-    </>
+
+
   );
 }
 
