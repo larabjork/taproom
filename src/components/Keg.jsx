c2ec66d816ca5185bb6ddf63432db276ad0f1d49
Lara Bjork <lara.m.bjork@gmail.com>
2020-02-15 22:00:53 -0800

    add styling to keg card, including pint sold button but display not solved

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import PropTypes from "prop-types";
-import { Card, CardDeck } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 
 const kegStyle = {
   backgroundColor: '#E59500',
@@ -9,40 +9,48 @@ const kegStyle = {
   fontFamily: 'Open Sans'
 }
 
+const btnStylePintSold= {
+  backgroundColor: '#002642',
+  borderColor: '#002642',
+  color: 'white'
+}
+
+
 function Keg(props){
   return (
     <div>
       <Card text="white" style={{ width: '18rem' }}>
         <Card.Body style={kegStyle}>
           <div>
-          <Card.Title>{props.name}</Card.Title>
+            <Card.Title>{props.name}</Card.Title>
             <style jsx>{`
-          .card-title {
-            font-family: 'Permanent Marker', cursive;
-            font-size: 56px;
-          }
-      `}
-    </style>
-          </div>
-          <Card.Text>
-            <p>Brewery: {props.brand}</p>
-            <p>Price: ${props.price}</p>
-            <p>Alcohol by Volume: {props.alcoholContent}</p>
-            <p>Pints Remaining: {props.pintRemaining} out of 124</p>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </div>
-  );
-}
+                .card-title {
+                  font-family: 'Permanent Marker', cursive;
+                  font-size: 56px;
+                }
+                `}
+              </style>
+            </div>
+            <Card.Text>
+              <p>Brewery: {props.brand}</p>
+              <p>Price: ${props.price}</p>
+              <p>Alcohol by Volume: {props.alcoholContent}</p>
+              <p>Pints Remaining: {props.pintRemaining} out of 124</p>
+            </Card.Text>
+            <Button style={btnStylePintSold}>1 Pint Sold!</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
 
-Keg.propTypes = {
-  name: PropTypes.string,
-  brand: PropTypes.string,
-  price: PropTypes.number,
-  alcoholContent: PropTypes.number,
-  pintRemaining: PropTypes.number
-};
+  Keg.propTypes = {
+    name: PropTypes.string,
+    brand: PropTypes.string,
+    price: PropTypes.number,
+    alcoholContent: PropTypes.number,
+    pintRemaining: PropTypes.number
+  };
 
 
-export default Keg;
+  export default Keg;
